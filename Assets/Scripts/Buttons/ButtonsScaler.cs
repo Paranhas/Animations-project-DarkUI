8eea060facdf23fa58ad63a82ee48f2eb5dc202e
Paranhas <rodrigoparanhas.br@gmail.com>
2026-02-17 14:45:19 -0300

    update aula 02

---
+++ Assets/Scripts/Buttons/ButtonsScaler.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+//using DG.Tweening;
+
+public class ButtonsScaler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+   // public float finalScale = 1.2f;
+   // public float scaleDuration = 0.1f;
+   // private Vector3 _defaultScale;
+
+    //private Tween _currentTween;
+
+   // private void Awake()
+   // {
+    //    _defaultScale = transform.localScale;
+   // }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Enter");
+        //_currentTween = transform.DOScale(_defaultScale * finalScale, scaleDuration);
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Exit");
+       // _currentTween.Kill();
+       // transform.localScale = _defaultScale;
+    }
+
+}
